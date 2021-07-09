c06fdc9ed2ba1e1d964bec205ae263c90b7029d8
Adityar Rayala <mailsofadityar@gmail.com>
2021-07-09 15:28:51 +0530

    add: folder structure, google sign in OAuth

--- src/App.js
+++ src/App.js
@@ -5,20 +5,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import React from "react";
+import { useSelector } from "react-redux";
 import Titlebar from "./Components/Titlebar";
 import Login from "./Pages/Login";
+import ChatSystem from "./Components/Chat";
+import CreatePost from "./Pages/CreatePost";
+// import Store from "./Store";
+// console.log();
 function App() {
+  const user = useSelector((state) => state.user);
+  console.log(user);
   return (
     <React.Fragment>
       <div className="App">
-        <Titlebar />
         <Router>
           <Switch>
             <Route exact path="/">
+              <Titlebar />
               <Home />
+              <ChatSystem />
             </Route>
             <Route exact path="/login">
+              <Titlebar />
+
               <Login />
+            </Route>
+            <Route exact path="/post/create">
+              <Titlebar />
+
+              <CreatePost />
+            </Route>
+            <Route>
+              <h3>404 not found</h3>
             </Route>
           </Switch>
         </Router>
